17015b7f373dbc2d6fba9ff6a2231bcba3874ba6
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-10-28 22:09:53 +0200

    fix: adjust gcc11 and gcc12 to work (same as gcc13)

---
+++ code/data/gcc12/applyFixes.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2023 S. G. Gottlieb <info.simon@gottliebtfreitag.de>
+# SPDX-License-Identifier: CC0-1.0
+
+cp -a --remove-destination data/gcc12/gcc ${TMP}/${name}/rootfs/usr/bin/gcc
+cp -a --remove-destination data/gcc12/g++ ${TMP}/${name}/rootfs/usr/bin/g++
